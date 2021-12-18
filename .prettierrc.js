ece959c688d6cbfc8c1e7ea401664585078f6e21
NachoToast <32235595+NachoToast@users.noreply.github.com>
2021-12-18 13:53:57 +1300

    styling enforcement

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    trailingComma: 'all',
+    printWidth: 100,
+    tabWidth: 4,
+    singleQuote: true,
+    arrowParens: 'always',
+    bracketSpacing: true,
+    semi: true,
+};
