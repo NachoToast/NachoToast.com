02f4a5893867778f118c0f857848df8dc37e8398
NachoToast <32235595+NachoToast@users.noreply.github.com>
2021-12-19 15:54:05 +1300

    alternating code backgrounds

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,33 @@
-import { Container } from '@mui/material';
+import { Container, Fade } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
+import backgrounds from './assets/code';
+import { useState } from 'react';
 
 function App() {
+    const [randomIndex] = useState(Math.floor(Math.random() * backgrounds.length));
+
     return (
-        <BrowserRouter>
-            <Container maxWidth="xl">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                </Routes>
-            </Container>
-        </BrowserRouter>
+        <Fade in>
+            <div
+                style={{
+                    width: '100%',
+                    height: '3000vh',
+                    backgroundImage: `url(${backgrounds[randomIndex]})`,
+                    backgroundColor: '#121212',
+                    backgroundRepeat: 'repeat-y',
+                    backgroundSize: '100%, auto',
+                }}
+            >
+                <BrowserRouter>
+                    <Container maxWidth="xl">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                        </Routes>
+                    </Container>
+                </BrowserRouter>
+            </div>
+        </Fade>
     );
 }
 
---
+++ client/src/assets/code/index.ts
@@ -0,0 +1,7 @@
+import homePage from './homePage.png';
+import mafiaHomeScreen from './mafiaHomeScreen.png';
+import nachoBotRconModule from './nachoBotRconModule.png';
+
+const allCodePages = [homePage, mafiaHomeScreen, nachoBotRconModule];
+
+export default allCodePages;
