7543f952af957118d793986d2cb526af6850212d
NachoToast <32235595+NachoToast@users.noreply.github.com>
2021-12-23 14:34:17 +1300

    persist speed dial if clicked on

--- client/src/components/QuickNavBar/QuickNavBar.tsx
+++ client/src/components/QuickNavBar/QuickNavBar.tsx
@@ -1,5 +1,5 @@
-import { SpeedDialIcon, SpeedDialAction, SpeedDial } from '@mui/material';
-import React from 'react';
+import { SpeedDialIcon, SpeedDialAction, SpeedDial, OpenReason, CloseReason } from '@mui/material';
+import React, { useState } from 'react';
 import HomeIcon from '@mui/icons-material/Home';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import { useNavigate } from 'react-router-dom';
@@ -7,9 +7,18 @@ import { useNavigate } from 'react-router-dom';
 const QuickNavBar = () => {
     const navigate = useNavigate();
 
-    function handleClick(e: React.MouseEvent) {
+    const [open, setOpen] = useState(false);
+
+    const [clickedOn, setClickedOn] = useState(false);
+
+    function handleOpen(e: React.SyntheticEvent<any>, reason: OpenReason) {
         e.preventDefault();
-        navigate('/');
+        setOpen(true);
+    }
+
+    function handleClose(e: React.SyntheticEvent<any>, reason: CloseReason) {
+        e.preventDefault();
+        setOpen(false);
     }
 
     return (
@@ -17,13 +26,23 @@ const QuickNavBar = () => {
             ariaLabel="Page speed dial"
             sx={{ position: 'fixed', bottom: 16, right: 16 }}
             icon={<SpeedDialIcon openIcon={<QuestionMarkIcon />} />}
+            onOpen={handleOpen}
+            onClose={handleClose}
+            open={clickedOn || open}
+            onClick={(e) => {
+                e.preventDefault();
+                setClickedOn(!clickedOn);
+            }}
         >
             <SpeedDialAction
                 key="home"
                 icon={<HomeIcon />}
                 tooltipTitle="Home"
                 // tooltipOpen
-                onClick={handleClick}
+                onClick={(e) => {
+                    e.preventDefault();
+                    navigate('/');
+                }}
             />
         </SpeedDial>
     );
